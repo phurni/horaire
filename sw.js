c0b34acef6cfc2a337faa10cf1c733b92c54cf1a
Pascal Hurni <phi@pragmaticpurist.org>
2019-09-16 21:31:22 +0200

    Add fetch handler for service worker, that was the missing bit to run as a PWA

--- sw.js
+++ sw.js
@@ -11,3 +11,12 @@ self.addEventListener('install', e => {
    })
  );
 });
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
