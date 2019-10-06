38968576b7a903410b98d4276f6c5ef8b0e4bd97
Pascal Hurni <phi@pragmaticpurist.org>
2019-10-06 11:37:37 +0200

    Put in cache the data for the current school year

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ self.addEventListener('install', e => {
        'assets/jquery-3.4.1.min.js',
        'assets/swipe.js',
        'assets/styles.css',
+       'data/config.json',
+       'data/1920-1C7-l1.txt',
      ]);
    })
  );
